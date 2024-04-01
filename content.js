11aae5d7d207bdd99e90aa94ab8f1c8db3cdcb1d
versx <versx@ver.sx>
2024-03-31 20:42:40 -0700

    fix skip button logic

--- content.js
+++ content.js
@@ -1,22 +1,36 @@
 function clickAd() {
   //const YouTubeSkipAdButtonSelector = '.ytp-ad-skip-button-modern.ytp-button';
-  const YouTubeSkipAdButtonSelector = '.ytp-ad-skip-button-modern';
+  const YouTubeSkipAdButtonSelector = '.ytp-ad-skip-button-modern.ytp-button';
   const skipButton = document.querySelector(YouTubeSkipAdButtonSelector);
   if (!skipButton) {
-    //console.log('No skip ad button found yet...');
     //console.log('Skip ad button not ready yet...');
+    setTimeout(clickAd, 1000);
+    return;
+  }
+
+  const video = document.querySelector('video');
+  if (!video) {
+    //console.log('video not found...');
+    setTimeout(clickAd, 1000);
+    return;
+  }
+
+  const canClickSkipButton = skipButton.checkVisibility({ visible: true });
+  if (!canClickSkipButton) {
+    console.log('Ad is still active, waiting...');
+    setTimeout(clickAd, 1000);
     return;
   }
 
   setTimeout(() => {
     console.log('Skip ad button found, clicking...');
     skipButton.click();
-  }, 500);
 
-  // Increment the total number of ads clicked
-  chrome.storage.sync.get(['totalAdsClicked'], function(data) {
-    chrome.storage.sync.set({ totalAdsClicked: data.totalAdsClicked + 1 });
-  });
+    // Increment the total number of ads clicked
+    chrome.storage.sync.get(['totalAdsClicked'], function(data) {
+      chrome.storage.sync.set({ totalAdsClicked: data.totalAdsClicked + 1 });
+    });
+  }, 500);
 
   // Check for 'Skip' ad button again
   setTimeout(clickAd, 1000);
